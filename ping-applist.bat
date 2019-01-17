6379c5e464351b88adc02a7930211acb3b2722be
benjimmie <43196027+benjimmie@users.noreply.github.com>
2019-01-17 12:31:58 +0800

    Update ping-applist.bat
    add pinglist.txt

--- ping-applist.bat
+++ ping-applist.bat
@@ -3,5 +3,5 @@ ECHO ===============================================
 ECHO  Hydra Connectivity Test
 ECHO  Pinging These IPs 10x  
 ECHO ===============================================
-for /f "delims=" %%a in (applist.txt) do (echo pinging %%a) && ping.exe -n 10 %%a >nul && (echo %%a OK) || (echo %%a NOK) 
+for /f "delims=" %%a in (pinglist.txt) do (echo pinging %%a) && ping.exe -n 10 %%a >nul && (echo %%a OK) || (echo %%a NOK) 
 pause
